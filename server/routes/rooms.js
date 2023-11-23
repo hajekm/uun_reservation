8348eef712723ff102e011fbc4ec048ff8f4503b
Martin Palas <martinpalas89@gmail.com>
2023-11-24 00:49:18 +0100

    fix: swagger

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -4,8 +4,67 @@ const rooms = require('../controllers/roomsController.js');
 const { ensureAuthenticated, isAdmin, isManager } = require('../middleware/authenticated.js');
 const validator = require('../middleware/validator.js');
 
+/**
+ * @swagger
+ * /rooms/list:
+ *   get:
+ *     summary: Get a list of all rooms
+ *     responses:
+ *       200:
+ *         description: List of rooms
+ *       500:
+ *         description: Internal server error
+ */
 router.get('/list', rooms.getAllRooms);
 
+/**
+ * @swagger
+ * /rooms/{roomId}:
+ *   put:
+ *     summary: Update a room by ID
+ *     parameters:
+ *       - in: path
+ *         name: roomId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: Unique ID of the room
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               room_number:
+ *                 type: integer
+ *                 description: Number of the room
+ *               type_id:
+ *                 type: integer
+ *                 description: ID of the room type
+ *               description:
+ *                 type: string
+ *                 description: Description of the room
+ *               price:
+ *                 type: number
+ *                 format: float
+ *                 description: Price of the room
+ *               beds:
+ *                 type: integer
+ *                 description: Number of beds in the room
+ *               options:
+ *                 type: string
+ *                 description: Additional options for the room
+ *     responses:
+ *       200:
+ *         description: Room updated successfully
+ *       400:
+ *         description: Invalid input
+ *       404:
+ *         description: Room not found
+ *       500:
+ *         description: Internal server error
+ */
 router.put('/:roomId',
     validator.updateRoomValidationRules,
     validator.validate,
@@ -13,8 +72,71 @@ router.put('/:roomId',
     rooms.updateRoom
 );
 
+/**
+ * @swagger
+ * /rooms/{roomId}:
+ *   get:
+ *     summary: Get a room by its ID
+ *     parameters:
+ *       - in: path
+ *         name: roomId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Unique ID of the room
+ *     responses:
+ *       200:
+ *         description: Details of the room
+ *       404:
+ *         description: Room not found
+ *       500:
+ *         description: Internal server error
+ */
 router.get('/:roomId', rooms.getRoomById);
 
+/**
+ * @swagger
+ * /rooms/create:
+ *   post:
+ *     summary: Create a new room
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - room_number
+ *               - description
+ *               - price
+ *             properties:
+ *               room_number:
+ *                 type: integer
+ *                 description: Number of the room
+ *               type_id:
+ *                 type: integer
+ *                 description: ID of the room type
+ *               description:
+ *                 type: string
+ *                 description: Description of the room
+ *               price:
+ *                 type: number
+ *                 format: float
+ *                 description: Price of the room
+ *               beds:
+ *                 type: integer
+ *                 description: Number of beds in the room
+ *               options:
+ *                 type: string
+ *                 description: Additional options for the room
+ *     responses:
+ *       201:
+ *         description: Room created successfully
+ *       400:
+ *         description: Invalid input
+ *       500:
+ *         description: Internal server error
+ */
 router.post('/create',
     validator.createRoomValidationRules,
     validator.validate,
@@ -22,6 +144,26 @@ router.post('/create',
     rooms.createRoom
 );
 
+/**
+ * @swagger
+ * /rooms/{roomId}:
+ *   delete:
+ *     summary: Delete a room by ID
+ *     parameters:
+ *       - in: path
+ *         name: roomId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Unique ID of the room
+ *     responses:
+ *       200:
+ *         description: Room deleted successfully
+ *       404:
+ *         description: Room not found
+ *       500:
+ *         description: Internal server error
+ */
 router.delete('/:roomId', isAdmin, rooms.deleteRoom);
 
 module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -4,8 +4,64 @@ const user = require('../controllers/userController.js');
 const { ensureAuthenticated, isAdmin, isManager } = require('../middleware/authenticated.js');
 const validator = require('../middleware/validator.js');
 
+/**
+ * @swagger
+ * /userslist:
+ *   get:
+ *     summary: Get a list of all users
+ *     security:
+ *       - adminAuth: []
+ *     responses:
+ *       200:
+ *         description: List of all users
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
 router.get('/list', isAdmin, user.getAllUsers);
 
+/**
+ * @swagger
+ * /users/{userId}:
+ *   put:
+ *     summary: Update a user by ID
+ *     security:
+ *       - adminAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Unique ID of the user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: Username of the user
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: Email address of the user
+ *               password:
+ *                 type: string
+ *                 description: Password for the user
+ *     responses:
+ *       200:
+ *         description: User updated successfully
+ *       400:
+ *         description: Validation error
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Internal server error
+ */
 router.put('/:userId',
     validator.updateUserValidationRules,
     validator.validate,
@@ -13,8 +69,65 @@ router.put('/:userId',
     user.updateUser
 );
 
+/**
+ * @swagger
+ * /users/{userId}:
+ *   get:
+ *     summary: Get a user by their ID
+ *     security:
+ *       - adminAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Unique ID of the user
+ *     responses:
+ *       200:
+ *         description: Details of the user
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Internal server error
+ */
 router.get('/:userId', isManager || isAdmin, user.getUserById);
 
+/**
+ * @swagger
+ * /users/create:
+ *   post:
+ *     summary: Create a new user
+ *     security:
+ *       - adminAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - username
+ *               - email
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: Username for the new user
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: Email address of the new user
+ *               password:
+ *                 type: string
+ *                 description: Password for the new user
+ *     responses:
+ *       201:
+ *         description: User created successfully
+ *       400:
+ *         description: Validation error
+ *       500:
+ *         description: Internal server error
+ */
 router.post('/create',
     validator.createUserValidationRules,
     validator.validate,
@@ -22,6 +135,28 @@ router.post('/create',
     user.createUser
 );
 
+/**
+ * @swagger
+ * /users/{userId}:
+ *   delete:
+ *     summary: Delete a user by ID
+ *     security:
+ *       - adminAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Unique ID of the user
+ *     responses:
+ *       200:
+ *         description: User deleted successfully
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Internal server error
+ */
 router.delete('/:userId', isAdmin, user.deleteUser);
 
 module.exports = router;
