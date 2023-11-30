e0b2e7d2e376006db36c2d79359ae146098e6ed9
Martin Palas <martinpalas89@gmail.com>
2023-11-30 23:58:24 +0100

    fix: application/json

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ app.use(cors());
 const swaggerSpec = swaggerJsdoc(config.swagger);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use((req, res, next) => {
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
     next();
 });
 
