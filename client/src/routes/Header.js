86931f4bdea54eb2e89c1a7e753a57ac784908a1
Milan Hajek <milan.hajek89@gmail.com>
2023-12-16 17:23:48 +0100

    dark mode cookie

--- client/src/routes/Header.js
+++ client/src/routes/Header.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faBed, faBook, faCalendarDay, faMoon, faSun, faUser} from "@fortawesome/free-solid-svg-icons";
 import {Menubar} from "primereact/menubar";
@@ -11,11 +11,13 @@ import {Button} from "primereact/button";
 import {ToggleButton} from "primereact/togglebutton";
 import {useTheme} from "../bricks/ThemeProvider";
 import {ReservationService} from "../Service";
+import Cookies from 'universal-cookie';
 
 function Header() {
     const googleAuthUrl = "https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=http%3A%2F%2Freservations.echovo.cz%2Fauth%2Fcallback&scope=profile%20email&client_id=657224920436-sk7p2u3drrib3drg2ni29t1atggi90nf.apps.googleusercontent.com";
     const [checked, setChecked] = useState(false)
     const {changeTheme} = useTheme();
+    const cookies = new Cookies();
 
 
     const items = [
@@ -35,6 +37,17 @@ function Header() {
             url: "/rooms",
         },
     ];
+
+    useEffect(() => {
+        const themeCookie = cookies.get('dark-theme');
+        if (!themeCookie) {
+            cookies.set('dark-theme', checked, {path: '/'});
+        } else {
+            setChecked(themeCookie)
+        }
+        changeTheme(checked ? "bootstrap4-dark-blue" : "bootstrap4-light-blue");
+    });
+
     const start = (
         <Link className="mr-8" to="/">
             <FontAwesomeIcon size={"3x"} icon={faBook}/>
@@ -47,6 +60,7 @@ function Header() {
             <ToggleButton className={"mr-3"} onIcon={<FontAwesomeIcon icon={faMoon}/>}
                           offIcon={<FontAwesomeIcon icon={faSun}/>}
                           checked={checked} onLabel="" offLabel="" onChange={(e) => {
+                cookies.set('dark-theme', !checked, {path: '/'});
                 setChecked(!checked);
                 changeTheme(checked ? "bootstrap4-light-blue" : "bootstrap4-dark-blue");
             }}/>
